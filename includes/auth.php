896821b982b0479c8ad8de60920ca23c24a855c0
Andres Elacion III <andreselacioniii@gmail.com>
2024-08-21 09:40:08 +0800

    added accntNumber, expDate and cvv upon register

--- includes/auth.php
+++ includes/auth.php
@@ -21,11 +21,19 @@
             if ($stmt->execute()) {
                 // Get the last inserted user_id
                 $user_id = $conn->lastInsertId();
+
+                // Generate card details
+                $account_number = generateUniqueAccountNumber();
+                $expiration_date = generateExpirationDate();
+                $cvv = generateCVV();
     
                 // Create an account for the new user
-                $account_query = "INSERT INTO accounts (user_id, balance) VALUES (:user_id, 0)";
+                $account_query = "INSERT INTO accounts (user_id, balance, account_number, expiration_date, cvv) VALUES (:user_id, 0, :account_number, :expiration_date, :cvv)";
                 $account_stmt = $conn->prepare($account_query);
                 $account_stmt->bindParam(':user_id', $user_id);
+                $account_stmt->bindParam(':account_number', $account_number);
+                $account_stmt->bindParam(':expiration_date', $expiration_date);
+                $account_stmt->bindParam(':cvv', $cvv);
                 $account_stmt->execute();
     
                 $conn->commit();
@@ -187,5 +195,34 @@
             $conn->rollback();
             return false;
         }
+    }
+
+    function generateUniqueAccountNumber() {
+        $db = new DB();
+        $conn = $db->connect();
+
+        do {
+            $accountNumber = rand(0000000000000000, 9999999999999999);
+
+            // check if the account number already exist
+            $query = "SELECT COUNT(*) FROM accounts WHERE account_number = :account_number";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':account_number', $accountNumber);
+            $stmt->execute();
+
+            $count = $stmt->fetchColumn();
+        } while ($count > 0);
+        return $accountNumber;
+    }
+
+    function generateExpirationDate() {
+        $date = new DateTime();
+        $date->add(new DateInterval('P3Y')); // This will set the expiration date 3 years from today
+
+        return $date->format('Y-m-d');
+    }
+
+    function generateCVV() {
+        return rand(300, 999);
     }
 ?>
