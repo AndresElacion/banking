05f39a99d66129b43e28589db8d208aa221794e2
Andres Elacion III <andreselacioniii@gmail.com>
2024-08-23 19:06:30 +0800

    configured nav

--- components/nav.php
+++ components/nav.php
@@ -4,78 +4,25 @@
       <h1 class="text-3xl text-center uppercase text-indigo-500">Banking App</h1>
     </div>
     <ul class="flex flex-col py-4">
-        <router-link to="/dashboard" custom v-slot="{ href, navigate }">
-          <li>
-            <a :href="href" @click="navigate" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-              <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class='bx bx-chevron-right'></i></span>
-              <span class="text-sm font-medium">Dashobard</span>
-            </a>
-          </li>
-        </router-link>
-        <router-link to="/enrollment" custom v-slot="{ href, navigate }">
-            <li>
-              <a :href="href" @click="navigate" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class='bx bx-chevron-right'></i></span>
-                <span class="text-sm font-medium">Enrollment</span>
-              </a>
-            </li>
-        </router-link>
-        <router-link to="/section" custom v-slot="{ href, navigate }">
-            <li>
-              <a :href="href" @click="navigate" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class='bx bx-chevron-right'></i></span>
-                <span class="text-sm font-medium">Section</span>
-              </a>
-            </li>
-        </router-link>
-        <router-link to="/term" custom v-slot="{ href, navigate}">
-          <li>
-            <a :href="href" @click="navigate" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-              <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class='bx bx-chevron-right'></i></span>
-              <span class="text-sm font-medium">Term</span>
-            </a>
-          </li>
-        </router-link>
-        <router-link to="/subject" custom v-slot="{ href, navigate }">
-          <li>
-            <a :href="href" @click="navigate" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-              <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class='bx bx-chevron-right'></i></span>
-              <span class="text-sm font-medium">Subjects</span>
-            </a>
-          </li>
-        </router-link>
-        <router-link to="/faculty" custom v-slot="{ href, navigate }">
-          <li>
-            <a :href="href" @click="navigate" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-              <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class='bx bx-chevron-right'></i></span>
-              <span class="text-sm font-medium">Facuty</span>
-            </a>
-          </li>
-        </router-link>
-        <router-link to="/student" custom v-slot="{ href, navigate }">
-          <li>
-            <a :href="href" @click="navigate" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-              <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class='bx bx-chevron-right'></i></span>
-              <span class="text-sm font-medium">Students</span>
-            </a>
-          </li>
-        </router-link>
-        <router-link to="/payment" custom v-slot="{ href, navigate }">
-          <li>
-            <a :href="href" @click="navigate" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-              <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class='bx bx-chevron-right'></i></span>
-              <span class="text-sm font-medium">Payments</span>
-            </a>
-          </li>
-        </router-link>
-        <router-link to="/" custom v-slot="{ href, navigate }" @click="signOut">
-          <li>
-            <a :href="href" @click="navigate" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-red-500">
-              <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class='bx bx-log-out'></i></span>
-              <span class="text-sm font-medium">Logout</span>
-            </a>
-          </li>
-        </router-link>
+      <li>
+        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class='bx bx-chevron-right'></i></span>
+          <span class="text-sm font-medium">Dashobard</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class='bx bx-chevron-right'></i></span>
+          <span class="text-sm font-medium">All Transaction</span>
+        </a>
+      </li>
+      <hr />
+      <li>
+        <a href="../public/logout.php" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-red-500">
+          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class='bx bx-log-out'></i></span>
+          <span class="text-sm font-medium">Logout</span>
+        </a>
+      </li>
     </ul>
 
     <!-- User Profile and Dropdown -->
@@ -92,16 +39,7 @@
         </div>
         <ul class="py-2" aria-labelledby="user-menu-button">
           <li>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Dashboard</a>
-          </li>
-          <li>
             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</a>
-          </li>
-          <li>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Earnings</a>
-          </li>
-          <li>
-            <a href="../public/logout.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Sign out</a>
           </li>
         </ul>
       </div>
