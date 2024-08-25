20be2402b6b3ae045b299ae632be8e661e95d62c
Andres Elacion III <andreselacioniii@gmail.com>
2024-08-25 15:01:07 +0800

    refactored

--- components/makeTransaction.php
+++
@@ -1,5 +0,0 @@
-<div class="rounded-2xl shadow-2xl p-6 mb-5 lg:mb-0 bg-white max-w-lg lg:max-w-md">
-    <p class="text-md font-bold">Balance:</p>
-    <p class="text-xl">$ <?php echo htmlspecialchars(number_format($accountDetails['balance'], 2)); ?></p>
-    <a href="transaction.php" class="rounded-xl bg-blue-500 text-white p-2 mt-5 inline-block hover:bg-blue-700">Make a Transaction</a>
-</div>
